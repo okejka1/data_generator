7804d616668af754c3b90b547171ee77182cdde8
okejka <miko02h@gmail.com>
2025-04-14 22:37:43 +0200

    models.py added

---
+++ main.py
@@ -0,0 +1,29 @@
+from faker import Faker
+
+# word = "python"
+#
+# # print(word[2:4])
+# print(word[4: ])
+# print(word[-2: ])
+# # print(word[4:0])
+# # print(word[:-2])
+# # print(word[:2])
+#
+# # Python strings cannot be changed — they are immutable.
+# # Therefore, assigning to an indexed position in the string results in an error:
+# new_word = word[:] + 'w'
+# print(new_word)
+# s = "qweq"
+# print(len(s))
+#
+# squares = [1,4,9,16,25]
+# squares.append(22)
+# squares[:] = []
+# print(squares)
+#
+#
+# a,b = 0,1
+# while a < 10:
+#     print(a)
+#     a, b=b, a+b
+#
---
+++ models.py
@@ -0,0 +1,123 @@
+from sqlalchemy import Column, Integer, String, Date, DECIMAL, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+
+class DepartmentResponsibility(Base):
+    __tablename__ = 'department_responsibility'
+
+    id = Column(Integer, primary_key=True)
+    employee_full_name = Column(String(255))
+    department_name = Column(String(255))
+    time_from = Column(DateTime)
+    time_to = Column(DateTime, nullable=True)
+    is_active = Column(Boolean)
+
+    appointments = relationship("Appointment", back_populates="department_responsibility")
+    documents = relationship("Document", back_populates="department_responsibility")
+
+
+class Patient(Base):
+    __tablename__ = 'patient'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(64))
+    last_name = Column(String(64))
+    pesel = Column(String(11), unique=True)
+    gender = Column(String(64))
+    date_of_birth = Column(Date)
+    address = Column(String(255))
+    email_address = Column(String(64), nullable=True)
+    phone_number = Column(String, nullable=True)
+
+    patient_cases = relationship("PatientCase", back_populates="patient")
+    documents = relationship("Document", back_populates="patient")
+
+
+class PatientCase(Base):
+    __tablename__ = 'patient_case'
+
+    id = Column(Integer, primary_key=True)
+    patient_id = Column(Integer, ForeignKey("patient.id"))
+    start_time = Column(DateTime)
+    end_time = Column(DateTime, nullable=True)
+    in_progress = Column(Boolean)
+    total_cost = Column(DECIMAL(10, 2), nullable=True)
+    amount_paid = Column(DECIMAL(10, 2), nullable=True)
+
+    patient = relationship("Patient", back_populates="patient_cases")
+    documents = relationship("Document", back_populates="patient_case")
+    appointments = relationship("Appointment", back_populates="patient_case")
+
+
+class Appointment(Base):
+    __tablename__ = 'appointment'
+
+    id = Column(Integer, primary_key=True)
+    patient_case_id = Column(Integer, ForeignKey("patient_case.id"))
+    department_responsibility_id = Column(Integer, ForeignKey("department_responsibility.id"))
+    time_created = Column(DateTime)
+    appointment_start_time = Column(DateTime)
+    appointment_end_time = Column(DateTime, nullable=True)
+    appointment_status_id = Column(Integer, ForeignKey("appointment_status.id"))
+
+    patient_case = relationship("PatientCase", back_populates="appointments")
+    department_responsibility = relationship("DepartmentResponsibility", back_populates="appointments")
+    appointment_status = relationship("AppointmentStatus", back_populates="appointments")
+    documents = relationship("Document", back_populates="appointment")
+    appointment_histories = relationship("AppointmentHistory", back_populates="appointment")
+
+
+class AppointmentStatus(Base):
+    __tablename__ = 'appointment_status'
+
+    id = Column(Integer, primary_key=True)
+    status_name = Column(String(64), unique=True)
+
+    appointments = relationship("Appointment", back_populates="appointment_status")
+    appointment_histories = relationship("AppointmentHistory", back_populates="appointment_status")
+
+
+class AppointmentHistory(Base):
+    __tablename__ = 'status_history'
+
+    id = Column(Integer, primary_key=True)
+    appointment_id = Column(Integer, ForeignKey("appointment.id"))
+    appointment_status_id = Column(Integer, ForeignKey("appointment_status.id"))
+    status_time = Column(DateTime)
+
+    appointment = relationship("Appointment", back_populates="appointment_histories")
+    appointment_status = relationship("AppointmentStatus", back_populates="appointment_histories")
+
+
+class DocumentType(Base):
+    __tablename__ = 'document_type'
+
+    id = Column(Integer, primary_key=True)
+    type_name = Column(String(64), unique=True)
+
+    documents = relationship("Document", back_populates="document_type")
+
+
+class Document(Base):
+    __tablename__ = 'document'
+
+    id = Column(Integer, primary_key=True)
+    document_internal_number = Column(Integer, unique=True)
+    document_name = Column(String)
+    time_created = Column(DateTime)
+    document_url = Column(String(255))
+    details = Column(String(1000), nullable=True)
+
+    patient_id = Column(Integer, ForeignKey("patient.id"), nullable=True)
+    patient_case_id = Column(Integer, ForeignKey("patient_case.id"), nullable=True)
+    department_responsibility_id = Column(Integer, ForeignKey("department_responsibility.id"), nullable=True)
+    appointment_id = Column(Integer, ForeignKey("appointment.id"), nullable=True)
+    document_type_id = Column(Integer, ForeignKey("document_type.id"), nullable=True)
+
+    patient = relationship("Patient", back_populates="documents")
+    patient_case = relationship("PatientCase", back_populates="documents")
+    department_responsibility = relationship("DepartmentResponsibility", back_populates="documents")
+    appointment = relationship("Appointment", back_populates="documents")
+    document_type = relationship("DocumentType", back_populates="documents")
