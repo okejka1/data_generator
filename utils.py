81b87297b5ee3ec04aa8ca5ee14acd8f8d14e62f
okejka <miko02h@gmail.com>
2025-05-13 22:02:14 +0200

    Changes by Antek

--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
+import random
+import string
+
 def generate_mail(name: str, last_name: str):
     return f'{name.lower()}.{last_name.lower()}@poczta.pl'
+
+def generate_document_name(document_type: str):
+    random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+    return f'{document_type.lower()}_{random_str}.pdf'
